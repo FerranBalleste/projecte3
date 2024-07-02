8a45e2d2c0234018ef543ce4da36489c4306e7df
FerranBalleste <fe.ba.solsona@gmail.com>
2024-07-02 15:45:16 +0200

    Registration and Home redirection

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth import authenticate, login
 from .models import *
 
 def home(request):
@@ -12,5 +13,17 @@ def product_detail(request, product_id):
   context = {'product': product, 'reviews': reviews}
   return render(request, 'product_detail.html', context)
 
-#def index(request):
-#    return HttpResponse("Hello, world. You're at the shop index.")
+def login_view(request):
+  if request.method == 'POST':
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(username=username, password=password)
+    if user:
+      login(request, user)
+      return render('home')  # Redirect to your home view
+    else:
+      # Return Invalid
+      pass
+  else:
+    # Display login form
+    return render(request, 'login.html')
